789d87b4000dd5b60545012690393535522977a0
Tung Pham <joseph.pham280996@gmail.com>
2024-12-06 13:25:01 -0500

    feat: update DQN implementation

---
+++ src/agents.py
@@ -0,0 +1,142 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import random
+import models as models
+import math
+
+from collections import deque, namedtuple
+
+Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class CartPoleAgent:
+    def __init__(self, 
+                 env, 
+                 state_dim, 
+                 action_dim, 
+                 device,
+                 batch_size = 128,
+                 epsilon = 0.9,
+                 epsilon_min = 0.05,
+                 epsilon_decay = 1000,
+                 tau = 0.005,
+                 gamma = 0.99,
+                 learning_rate = 1e-4):
+        self.device = device
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.env = env
+
+        # Hyperparameters
+        self.batch_size = batch_size
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        self.tau = tau
+        self.gamma = gamma
+        self.learning_rate = learning_rate
+
+        self.memory = ReplayMemory(10000)
+
+        self.policy_net = models.DQN(state_dim, action_dim).to(self.device)
+        self.target_net = models.DQN(state_dim, action_dim).to(self.device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+
+        self.optimizer = optim.Adam(
+            self.policy_net.parameters(), lr=self.learning_rate, amsgrad=True
+        )
+        self.criterion = nn.MSELoss()
+
+        self.steps_done = 0
+
+    def memorize_observation(self, state, action, next_state, reward):
+        self.memory.push(state, action, next_state, reward)
+
+
+    def select_action(self, state):
+        sample = random.random()
+        eps_threshold = self.epsilon_min + (self.epsilon - self.epsilon_min) * math.exp(
+            -1.0 * self.steps_done / self.epsilon_decay
+        )
+
+        self.steps_done += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                return self.policy_net(state).max(1).indices.view(1, 1)
+        else:
+            return torch.tensor(
+                [[self.env.action_space.sample()]], device=self.device, dtype=torch.long
+            )
+
+    def optimize(self):
+        if len(self.memory) < self.batch_size:
+            return
+        transitions = self.memory.sample(self.batch_size)
+        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+        # detailed explanation). This converts batch-array of Transitions
+        # to Transition of batch-arrays.
+        batch = Transition(*zip(*transitions))
+
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                            batch.next_state)), device=self.device, dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                                    if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1).values
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(self.batch_size, device=self.device)
+        with torch.no_grad():
+            next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1).values
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+
+        # Compute Huber loss
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer.step() 
+
+    def update(self):
+        target_net_state_dict = self.target_net.state_dict()
+        policy_net_state_dict = self.policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[
+                key
+            ] * self.tau + target_net_state_dict[key] * (1 - self.tau)
+        self.target_net.load_state_dict(target_net_state_dict)
---
+++ src/train.py
@@ -0,0 +1,57 @@
+import numpy as np
+from itertools import count
+from agents import CartPoleAgent
+import torch
+from tqdm import tqdm 
+
+def train(env, agent:CartPoleAgent, device):
+    tr_info = {'durations': [], 'total_rewards': []}
+
+    if torch.cuda.is_available() or torch.backends.mps.is_available():
+        num_episodes = 600
+    else:
+        num_episodes = 50
+
+    progress_bar = tqdm(range(num_episodes))
+    pbar_info = {}
+
+
+    for i_episode in progress_bar:
+        # Initialize the environment and get its state
+        total_reward = 0
+        state, _ = env.reset()
+        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+        for t in count():
+            action = agent.select_action(state)
+            observation, reward, terminated, truncated, _ = env.step(action.item())
+            total_reward += reward
+            reward = torch.tensor([reward], device=device)
+            done = terminated or truncated
+
+            if terminated:
+                next_state = None
+            else:
+                next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+
+            # Store the transition in memory
+            agent.memorize_observation(state, action, next_state, reward)
+
+            # Move to the next state
+            state = next_state
+
+            # Perform one step of the optimization (on the policy network)
+            agent.optimize()
+            
+            if done:
+                tr_info['durations'].append(t + 1)
+                tr_info['total_rewards'].append(total_reward)
+                pbar_info.update({
+                    'total_r': total_reward,
+                    'duration': t + 1
+                })
+                break
+
+        progress_bar.set_postfix(pbar_info)
+
+    progress_bar.close()
+    return tr_info
